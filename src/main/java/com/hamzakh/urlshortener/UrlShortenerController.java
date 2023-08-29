c5aa299f5f5912896edf5e2357a12815dbc63759
HamzaELKhatib <hamza.elkhatib.8@outlook.com>
2023-08-29 14:20:58 +0100

    feat: Moving Application logic to the service layer

--- src/main/java/com/hamzakh/urlshortener/UrlShortenerController.java
+++ src/main/java/com/hamzakh/urlshortener/UrlShortenerController.java
@@ -1,13 +1,10 @@
 package com.hamzakh.urlshortener;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.security.NoSuchAlgorithmException;
 
 @RequestMapping("/rest/url")
@@ -15,31 +12,15 @@ import java.security.NoSuchAlgorithmException;
 public class UrlShortenerController {
 
     @Autowired
-    StringRedisTemplate redisTemplate;
-
-    @Autowired
     UrlShortenerService urlShortenerService;
 
     @GetMapping("/{hash}")
     public ResponseEntity<HttpStatus> resolve(@PathVariable String hash) {
-
-        final String url = redisTemplate.opsForValue().get(hash);
-
-        if (url == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity
-                .status(HttpStatus.MOVED_PERMANENTLY)
-                .location(URI.create(url))
-                .header(HttpHeaders.CONNECTION, "close")
-                .build();
+        return urlShortenerService.retrieveAndRedirect(hash);
     }
 
     @PostMapping
     public String shorten(@RequestBody String url) throws NoSuchAlgorithmException {
-        var hash = urlShortenerService.hash(url).substring(0, 6);
-        redisTemplate.opsForValue().set(hash, url);
-        return hash;
+        return urlShortenerService.cacheAndShorten(url);
     }
 }
--- src/main/java/com/hamzakh/urlshortener/UrlShortenerService.java
+++ src/main/java/com/hamzakh/urlshortener/UrlShortenerService.java
@@ -1,16 +1,25 @@
 package com.hamzakh.urlshortener;
 
 import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
+import java.net.URI;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 @Service
 public class UrlShortenerService {
 
-    public String hash(String url) throws NoSuchAlgorithmException {
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    public String hash(final String url) throws NoSuchAlgorithmException {
 
         var urlValidator = new UrlValidator(new String[]{"http", "https"});
 
@@ -22,10 +31,32 @@ public class UrlShortenerService {
 
         var bytes = digest.digest(url.getBytes());
 
-        var hash = String.format("%32x", new BigInteger(1, bytes));
+        return String.format("%32x", new BigInteger(1, bytes));
+
+
+    }
+
+    public String cacheAndShorten(final String url) throws NoSuchAlgorithmException {
+
+        var hash = this.hash(url).substring(0, 6);
+
+        redisTemplate.opsForValue().set(hash, url);
 
         return hash;
+    }
 
+    public ResponseEntity<HttpStatus> retrieveAndRedirect(final String hash) {
 
+        final String url = redisTemplate.opsForValue().get(hash);
+
+        if (url == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.MOVED_PERMANENTLY)
+                .location(URI.create(url))
+                .header(HttpHeaders.CONNECTION, "close")
+                .build();
     }
 }
